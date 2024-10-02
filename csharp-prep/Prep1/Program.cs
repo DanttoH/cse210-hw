64bf9c69546f1cc9b3341cb9664f8d6e2861670d
DanttoH <her23001@byui.edu>
2024-10-01 23:35:13 -0300

    prep1changed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+         Console.Write("What is your first name? ");
+         string fname = Console.Readline();
+
+         Console.Write("What is your last name? ");
+         string lname = Console.Readline();
+         
+         Console.Writeline($"Your name is {lname}, {fname}{lname}.");
+
     }
 }
